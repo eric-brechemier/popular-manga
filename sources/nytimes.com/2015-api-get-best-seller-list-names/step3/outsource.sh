6e1b934c3484760cea244ae7c0b8a4149a4b95cb
Eric Bréchemier <eric@bezen.org>
2015-03-19 14:37:30 +0100

    Prepare implementation of throttling (to limit rate of requests)
    I planned to measure the time of each request and ensure that
    no more than the maximum of 8 requests were sent in a floating
    window of 1 second. This requires to measure the time of each
    request with subsecond accuracy and to maintain a list of the
    times measured for up to the maximum number of requests allowed.
    
    However, POSIX utilities only offer an accuracy of 1 second,
    which requires to stretch the floating window to 2 seconds,
    or to halve the allowed rate to 4 requests per second. These
    two methods will result in similar request rates in average,
    but the latter is simpler to implement and reduces the maximum
    request rate observed in a floating window.
    
    I will thus simplify the logic on these lines:
    * count the number of requests in current second
    * reset the counter when the second changes

--- sources/nytimes.com/2015-api-get-best-seller-list-names/step3/outsource.sh
+++ sources/nytimes.com/2015-api-get-best-seller-list-names/step3/outsource.sh
@@ -2,6 +2,24 @@
 # Requires csvcut, from csvkit (0.9.0)
 cd "$(dirname "$0")"
 
+# Return the number of seconds since the start of current hour
+seconds_in_hour()
+{
+  echo "$(date +'%M * 60 + %S' | bc)"
+}
+
+# Return the difference between two numbers of seconds in an hour.
+# The second value is considered to correspond to a date less than
+# one hour later than the first value, which allows to compute the
+# difference across hour boundaries.
+# This method is intended to measure intervals ranging from less than
+# a second (but without the subsecond granularity) to a few seconds.
+# It remains valid for intervals smaller than 1 hour.
+delta_seconds_in_hour()
+{
+  echo "$(( ( $2 - $1 + 3600 ) % 3600 ))"
+}
+
 tail -n 1 ../data.csv |
 csvcut -c 4,5,6 |
 csvformat -T |
@@ -24,6 +42,10 @@ then
 
   echo "Julian Day Start: $julianStartDay"
   julianDay="$julianStartDay"
+
+  startSeconds="$(seconds_in_hour)"
+  counter=0
+
   # TODO: check if the newest published date (in the future) must be skipped;
   #       in this case, use the while loop with the test below
   #       instead of the until loop.
@@ -31,6 +53,11 @@ then
   until test "$julianDay" -gt "$julianEndDay"
   do
     echo "Julian Day: $julianDay"
+
+    counter=$(( $counter + 1 ))
+    currentSeconds="$(seconds_in_hour)"
+    interval=$( delta_seconds_in_hour $startSeconds $currentSeconds )
+    echo "Rate: $(( $counter / (1+$interval) )) / second"
 
     isoDate="$( ./julian2iso.sh "$julianDay")"
     echo "ISO Date: $isoDate"
